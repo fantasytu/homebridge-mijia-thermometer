3725512f116def77a3071cfea4e765f4cba5063b
Fantasy Tu <f.tu@me.com>
2021-08-13 16:56:23 +0800

    patch

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 import { API } from 'homebridge';
-
-import { ACCESSORY_NAME } from './settings';
+import { ACCESSORY_NAME, PLUGIN_NAME } from './settings';
 import { MijiaThermometerAccessory } from './accessory';
 
 /**
  * This method registers the platform with Homebridge
  */
 export = (api: API) => {
-  api.registerAccessory(ACCESSORY_NAME, MijiaThermometerAccessory);
+  api.registerAccessory(PLUGIN_NAME, ACCESSORY_NAME, MijiaThermometerAccessory);
 };
